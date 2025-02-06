18ec73fe2b05bc5eac505e072ceba206126ea5d5
shuri.liuth <shuri.liuth@shopee.com>
2025-02-06 14:37:27 +0800

    feat: layout phase 2

--- src/components/Avatar/AvatarControlPanel.tsx
+++ src/components/Avatar/AvatarControlPanel.tsx
@@ -6,25 +6,42 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
+import { Button } from "@/components/ui/button";
 import { Label } from "@radix-ui/react-label";
 import { Textarea } from "../ui/textarea";
+import { Input } from "@/components/ui/input";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { Checkbox } from "@/components/ui/checkbox";
+import SettingDialog from "./SettingDialog";
 
 export default function AvatarControlPanel() {
   return (
     <>
       <CardHeader>
         <CardTitle>Avatar Control Panel</CardTitle>
+        <CardDescription>
+          Converts your input text into speech in one-click.
+        </CardDescription>
       </CardHeader>
       <CardContent>
         <form>
-          <div className="grid w-full items-center gap-4">
+          <div>
             <div className="flex flex-col space-y-1.5">
               <Label htmlFor="spokenText">Spoken Text</Label>
               <Textarea
                 placeholder="Type your message here."
                 id="spokenText"
-                className="resize-none"
+                className="h-56 resize-none"
               />
+            </div>
+            <div className="flex justify-center mt-2">
+              <SettingDialog />
             </div>
           </div>
         </form>
---
+++ src/components/Avatar/SettingDialog.tsx
@@ -0,0 +1,13 @@
+import { Settings } from "lucide-react";
+import { Button } from "../ui/button";
+
+export default function SettingDialog() {
+  return (
+    <>
+      <Button variant="secondary">
+        Settings
+        {/* <Settings /> */}
+      </Button>
+    </>
+  );
+}
