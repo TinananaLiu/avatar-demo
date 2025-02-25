f7bc2eeba2852c911930d6e117e5314f8dbf2beb
shuri.liuth <shuri.liuth@shopee.com>
2025-02-25 17:15:02 +0800

    feat: avatar initialization

--- src/components/Avatar/AvatarPanel.tsx
+++ src/components/Avatar/AvatarPanel.tsx
@@ -1,14 +1,116 @@
 import * as SpeechSDK from "microsoft-cognitiveservices-speech-sdk";
-import { useEffect } from "react";
+import axios from "axios";
+import { useEffect, useState } from "react";
 import useAvatarStore from "@/store/useAvatarStore";
 import { get } from "http";
 
 function AvatarPanel() {
-  const { azureSpeech } = useAvatarStore();
+  const { azureSpeech, ttsConfig, avatarConfig } = useAvatarStore();
+  const [avatarSynthesizer, setAvatarSynthesizer] =
+    useState<SpeechSDK.AvatarSynthesizer | null>(null);
+  const [peerConnection, setPeerConnection] =
+    useState<RTCPeerConnection | null>(null);
+  const [isSessionActive, setIsSessionActive] = useState(false);
 
-  useEffect(() => {
-    if (!azureSpeech.APIKey || !azureSpeech.region) return;
-  }, []);
+  const startSession = async () => {
+    if (!azureSpeech.APIKey || !azureSpeech.region) {
+      alert("No API Key & Region!");
+      return;
+    }
+
+    setIsSessionActive(true);
+
+    try {
+      const speechSynthesisConfig = azureSpeech.enablePrivateEndpoint
+        ? SpeechSDK.SpeechConfig.fromEndpoint(
+            new URL(
+              `wss://${azureSpeech.privateEndpoint}/tts/cognitiveservices/websocket/v1?enableTalkingAvatar=true`
+            ),
+            azureSpeech.APIKey
+          )
+        : SpeechSDK.SpeechConfig.fromSubscription(
+            azureSpeech.APIKey,
+            azureSpeech.region
+          );
+
+      speechSynthesisConfig.endpointId = ttsConfig.customVoiceEndpointId;
+
+      // avatar video format
+      const videoFormat = new SpeechSDK.AvatarVideoFormat();
+      // ...
+
+      // avatar params
+      const avatarConfiguration = new SpeechSDK.AvatarConfig(
+        avatarConfig.talkingAvatarCharacter,
+        avatarConfig.talkingAvatarStyle,
+        videoFormat
+      );
+      avatarConfiguration.customized = avatarConfig.customizedAvatar;
+      avatarConfiguration.backgroundColor = avatarConfig.backgroundColor;
+      avatarConfiguration.backgroundImage = new URL(
+        avatarConfig.backgroundImageUrl
+      );
+
+      // GET api
+      const requestUrl = azureSpeech.enablePrivateEndpoint
+        ? `https://${azureSpeech.privateEndpoint}/tts/cognitiveservices/avatar/relay/token/v1`
+        : `https://${azureSpeech.region}.tts.speech.microsoft.com/cognitiveservices/avatar/relay/token/v1`;
+
+      const response = await axios.get(requestUrl, {
+        headers: { "Ocp-Apim-Subscription-Key": azureSpeech.APIKey },
+      });
+
+      const { Urls, Username, Password } = response.data;
+      const iceServerUrl = Urls[0];
+      const iceServerUsername = Username;
+      const iceServerCredential = Password;
+
+      avatarConfiguration.remoteIceServers = [
+        {
+          urls: [iceServerUrl],
+          username: iceServerUsername,
+          credential: iceServerCredential,
+        },
+      ];
+
+      const synthesizer = new SpeechSDK.AvatarSynthesizer(
+        speechSynthesisConfig,
+        avatarConfiguration
+      );
+
+      synthesizer.avatarEventReceived = (s, e) => {
+        const offsetMessage =
+          e.offset === 0
+            ? ""
+            : `, offset from session start: ${e.offset / 10000}ms.`;
+        console.log(
+          `[${new Date().toISOString()}] Event received: ${
+            e.description
+          }${offsetMessage}`
+        );
+      };
+
+      setAvatarSynthesizer(synthesizer);
+    } catch (error) {}
+  };
+
+  const setupWebRTC = (
+    iceServerUrl: string,
+    iceServerUsername: string,
+    iceServerCredential: string
+  ) => {
+    const pc = new RTCPeerConnection({
+      iceServers: [
+        {
+          urls: [iceServerUrl],
+          username: iceServerUsername,
+          credential: iceServerCredential,
+        },
+      ],
+    });
+
+    pc.ontrack = (event) => {};
+  };
 
   return <></>;
 }
